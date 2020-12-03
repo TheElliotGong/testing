d525d2313ae7e8f26fb41014ebafba7d21d80de1
Elliot Gong <ewcgong@hotmail.com>
2020-12-02 22:03:36 -0800

    tried pushing to my personal repo on github

---
+++ my demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 10;
+            Console.Write("Hellow");
+        }
+    }
+}
