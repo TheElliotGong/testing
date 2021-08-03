e3754ef82ee1fa9e854f75328a168ff7ef5aa458
elliot gong <ewcgong@hotmail.com>
2021-08-03 13:54:52 -0700

    created the assignIcontoSquares method

--- Reworked Matching Game/Form1.cs
+++ Reworked Matching Game/Form1.cs
@@ -21,9 +21,29 @@ namespace Reworked_Matching_Game
             "!", "!", "N", "N", ",", ",", "k", "k",
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
+
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = rng.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    // iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
         public MatchingGame()
         {
             InitializeComponent();
+            AssignIconsToSquares();
         }
 
         private void MatchingGame_Load(object sender, EventArgs e)
